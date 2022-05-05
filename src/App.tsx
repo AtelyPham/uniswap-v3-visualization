eb2eae269d3f66a160ec033137d7fde54fc1c575
AtelyPham <tinpham1412000@gmail.com>
2022-05-06 01:57:16 +0700

    feat: add pre-commit checking for commit convention

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function App() {
   return (
-    <h1 className='text-5xl text-center font-semibold text-blue-500'>
+    <h1 className="text-5xl text-center font-semibold text-blue-500">
       Hello world
     </h1>
-  )
+  );
 }
 
-export default App
+export default App;
